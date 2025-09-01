1145f6ab929e222a27da7bd622ec2455543cb2fa
Rohit Kumar <147086495+rohit-codes7@users.noreply.github.com>
2025-09-01 23:57:44 +0530

    Time: 1270 ms (10.06%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -7,4 +7,4 @@ Left Join Examinations e
 on s.student_id = e.student_id
 And sub.subject_name = e.subject_name
 Group by s.student_id, s.student_name, sub.subject_name
-Order by s.student_id;
+Order by s.student_id, sub.subject_name;
